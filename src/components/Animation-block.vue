97ef99f9d9861a6d2eb134387f3297c1d77b11bc
Ilya <ilya.w.a.j@gmail.com>
2022-11-24 10:29:24 +0400

    update animation block

--- src/components/Animation-block.vue
+++ src/components/Animation-block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="el" class="relative bg-[#111827] h-[500vh] z-[999999] rounded px-12  mx-auto">
+    <div ref="el" class="relative bg-[#111827] h-[600vh] z-[999999] rounded px-12  mx-auto">
     <div class=" mt-16 h-fit bg-white rounded-[32px] sticky z-[999] top-[5vh] border-[#232d3f] border-[13px]  ">
         <div  v-if="y <= 3510" id="animatemainBlock"   class="w-[60rem] rounded-[32px] py-2 px-3   bg-white relative z-[888]">
             <animateHeaderVue  :y="y" class="animation_header relative z-[9999] transition-all"  id="header" />
@@ -8,12 +8,28 @@
                 <contentBlock id="contentBlock" />
             </div>
             <generate id='generateBlock'/>
-            <article1 v-if="y >= 2992" class="z-[999999999]"  id="article1" />
+          
         </div>
-        <div v-else  id="animationBlockPage2" class="w-[60rem] overflow-hidden h-[542px] rounded-[32px] py-2 px-3 bg-white relative">
+        <div v-else  id="animationBlockPage" class="w-[60rem]  h-[542px] rounded-[32px] py-2 px-3 bg-white relative">
             <animateHeader2Vue />
-            <contentBlock2 />
+            <div class="overflow-hidden h-[500px]">
+            <contentBlock2 id="contentBlock2" />
+            </div>
+            <article2  class="z-[999999999]"/>
+            <div class="social-medias flex flex-col top-[60%]  absolute">
+                <a href="/" class="bg-[#6b7280] mb-3 rounded-full w-8 flex justify-center p-2">
+                    <img  src="https://uploads-ssl.webflow.com/627a1044a798e6627445c8d1/6291e61b354f09e25db94135_facebook.svg" alt="">
+                </a>
+                <a href="/" class="bg-[#6b7280] mb-3 rounded-full w-8 flex justify-center p-2">
+                    <img class="w-4" src="https://uploads-ssl.webflow.com/627a1044a798e6627445c8d1/6291e61a354f097c76b940f4_twitter.svg" alt="">
+                </a>
+                <a href="/" class="bg-[#6b7280] mb-3 rounded-full w-8 flex justify-center p-2">
+                    <img class="w-4" src="https://uploads-ssl.webflow.com/627a1044a798e6627445c8d1/6291e61a354f09d0eeb94113_instagram.svg" alt="">
+                </a>
+            </div>
+            <img class=" w-14 absolute -bottom-4 left-20" src="https://uploads-ssl.webflow.com/627a1044a798e6627445c8d1/627a1045a798e6056545ca5a_rose-check.png" alt="">
         </div>
+        <article1 v-if="y >= 2992" class="z-[999999999]"  id="article1" />
     </div> 
     </div>
 </template>
@@ -24,6 +40,7 @@ import sidebar from "./animations/sidebar.vue";
 import contentBlock from "./animations/content-block.vue"
 import generate from "./animations/generate.vue";
 import article1 from "./animations/article-1.vue";
+import article2 from "./animations/article-2.vue";
 import contentBlock2 from './animations/content_blocks/content-6.vue'
 import { useScroll,useWindowScroll} from '@vueuse/core'
 import { onMounted,ref } from "vue";
@@ -69,7 +86,7 @@ onMounted(() => {
                    }
                    if(contentAnimateBlockPosition >= startAnimatePosition ){
                         sidebarBlockPosition = contentAnimateBlockPosition - startAnimatePosition
-                        let sidbarBlockFinalyPosition = 492
+                        var sidbarBlockFinalyPosition = 492
                         if(sidebarBlockPosition >= sidbarBlockFinalyPosition){
                             sidebarBlockPosition = sidbarBlockFinalyPosition
                             sidebarBlockPosition / 10   
@@ -85,6 +102,26 @@ onMounted(() => {
                 else{
                     mainBlockOpacity  =1
                 }
+                if(windowY >= 599){
+                var animateBlockOpacity= 0
+                var contentAnimateBlockPosition2 = 0
+                    animateBlockOpacity += (windowY /500 ) - 1.10
+                    if(this.document.getElementById('animationBlockPage')){
+                           this.document.getElementById('animationBlockPage').style.opacity =`${animateBlockOpacity}`
+                           this.document.getElementById('contentBlock2').style.transform = `translateY(-${contentAnimateBlockPosition2.toFixed(0)}px)`
+                         }
+                         if(contentAnimateBlockPosition2 >= 328){
+                            contentAnimateBlockPosition2 = 328
+                           }
+                    if(windowY >= 1075){
+                        contentAnimateBlockPosition2 += (windowY - 1075)
+                        if(contentAnimateBlockPosition2 >= 328){
+                            contentAnimateBlockPosition2 = 328
+                           }
+                        this.document.getElementById('contentBlock2').style.transform = `translateY(-${contentAnimateBlockPosition2}px)`
+                        }
+                }
+                console.log(contentAnimateBlockPosition2);
                 this.document.getElementById('header').style.transform = ` scale(${headerBlockScale}) translateY(-${headerBlockPosition}px)`
                 this.document.getElementById('contentBlock').style.transform = `translateY(-${contentAnimateBlockPosition.toFixed(0)}px)`
                 this.document.getElementById('generateBlock').style.transform = `scale(${generateBlockPosition})`
@@ -92,8 +129,6 @@ onMounted(() => {
                 this.document.getElementById('article1').style.transform = `translateY(-${modalYPosition}px)`
                 this.document.getElementById('article1').style.opacity = `${modalOpacity}`
                 this.document.getElementById('animatemainBlock').style.opacity = `${mainBlockOpacity}`
-                // console.log(windowY);
-                console.log(y)
           });
       })
 
---
+++ src/components/animations/article-2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="article-2 bg-white absolute -left-16 top-28  flex flex-col rounded-2xl w-64  shadow-xl p-2 ">
+        <div class="flex mt-2 items-center justify-center flex-col">
+            <img class=" rounded-full w-10 h-9" src="https://img.freepik.com/premium-photo/egoistic-teen-girl-wear-diadem-arrogance-selfishness-portrait-glory-cheerful-child-wear-crown-self-confident-queen-expressing-smug-arrogant-princess-tiara-proud-kid-with-curly-hair_474717-11288.jpg?w=1060" alt="">
+            <h1>Jane Cooper</h1>
+            <p class="text-sm text-[#7d8690]">May 12 - 6 min read</p>
+        </div>
+        <div class="mt-2 bg-[#f9fafb] text-center w-full rounded-xl">
+            <p class="text-center">32 14 12 847</p>
+        </div>
+    </div>
+</template>
