9a383a016896c790f111407403f29f59cb3604ce
Ilya <ilya.w.a.j@gmail.com>
2022-11-26 00:13:05 +0400

    wrapper-feature

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@
     <section class="section-1 relative -mt-20 pt-28 sm:pt-20 pb-14 flex flex-[1] z-20">
       <Section_1 />
     </section>
+    <section class="bg-white rounded-t-[48px] py-11">
+      <Feature />
+    </section>
   </div>
 </template>
-<script>
+<script setup>
 import Navbar  from "./components/Navbar.vue";
 import Section_1  from "./components/sections/section-1.vue";
-export default {
-  components: { Navbar,Section_1}
-}
+import Feature from './components/sections/section-feature.vue'
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       animation: {
         'horizontal': 'marqueehorizontal 90s linear infinite;',
       },
+      backgroundImage: {
+        'feature-hero': "url('https://uploads-ssl.webflow.com/627a1044a798e6627445c8d1/627a1045a798e621b745c9a8_bg-gradient.png')",
+    },
     },
   },
   plugins: [],
